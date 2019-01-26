91b6d9d9d97571d60df1719962757efec76d3532
DESKTOP-L4LLM8G\60294 <602941661@qq.com>
2019-01-27 01:02:40 +0800

    我想玩 只狼

---
+++ GGJ2019/Assets/Scripts/AudioSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSystem : MonoBehaviour
+{
+    private AudioSource AS;
+
+    public AudioClip Attack;
+    public AudioClip BeAttacked;
+    public AudioClip Rope;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    public void PlayAttack()
+    {
+        AS.PlayOneShot(Attack);
+    }
+    public void PlayBeAttacked()
+    {
+        AS.PlayOneShot(BeAttacked);
+    }
+    public void PlayRope()
+    {
+        AS.PlayOneShot(Rope);
+    }
+}
