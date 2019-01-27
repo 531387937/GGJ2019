ee94588981e2b66bd9326c3b8ed4c4adf51b7ede
Zoryx <15801610913@163.com>
2019-01-27 10:34:15 +0800

    攻击

---
+++ GGJ2019/Assets/PlayerATk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerATk : MonoBehaviour
+{
+    private bool damage;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        damage = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Enemy"&&!damage)
+        {
+            damage = true;
+            collision.gameObject.SendMessage("BeAttacked");
+        }
+    }
+}
