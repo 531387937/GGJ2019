c368894b58d4a0b78cf3a65be160162a0fe8bd88
DESKTOP-L4LLM8G\60294 <602941661@qq.com>
2019-01-27 10:33:52 +0800

    no message

--- GGJ2019/Assets/Scripts/Enemy/BlueGhost.cs
+++ GGJ2019/Assets/Scripts/Enemy/BlueGhost.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime : MonoBehaviour
+public class BlueGhost : Enemy
 {
-    public Slime_ScriptableObject slime_data;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
---
+++ GGJ2019/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected int Health;
+    protected Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void BeAttacked()
+    {
+        Health--;
+    }
+
+    public void BeHooked(Animator animator)
+    {
+        animator.SetBool("Hooked", true);
+        StartCoroutine(HookTime(animator));
+    }
+
+    IEnumerator HookTime(Animator animator)
+    {
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("Hooked", false);
+    }
+
+    public void BeDead(Animator animator)
+    {
+        animator.SetBool("Dead", true);
+    }
+
+    public void CheckDead()
+    {
+        if (Health <= 0)
+            BeDead(GetComponent<Animator>());
+    }
+}
